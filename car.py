e4785e7589aacb798c6f7e7fe326b98cd0ee23f0
Julian Toelke <julian.toelki@web.de>
2025-10-18 10:37:18 +0200

    added the car class

---
+++ car.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import math
+from numba import jit
+
+from vehicle_visual import degreeToRadians
+from vehicle_visual import static_axle_visual
+from vehicle_visual import parallel_axle_visual
+
+x = 0
+y = 1
+
+
+class car:
+    def __init__(self, length, width, wheel_length, wheel_width, line_width, start_x, start_y, alignment_start):
+        self.length = length
+        self.width = width
+        self.line_width = line_width
+        self.position_front = (start_x, start_y)
+        self.alignment = alignment_start
+        self.position_back = (0, 0)
+        self.static_axle = static_axle_visual(width, wheel_length, wheel_width, line_width)
+        self.parallel_axle = parallel_axle_visual(width, wheel_length, wheel_width, line_width)
+
+    def draw(self, image, distance, alignment_wheel):
+        front_deviation = self.__getFrontDeviation(distance, alignment_wheel)
+        right_deviation = self.__getRightDeviation(distance, alignment_wheel)
+        self.position_front = self.__getNewFrontPosition(self.alignment, front_deviation, right_deviation)
+        self.alignment = self.alignment + self.__getDistanceAngle(distance, alignment_wheel)
+        self.position_back = self.__getBackPosition()
+
+        # middle bar
+        cv2.line(image, (int(self.position_front[x]),int(self.position_front[y])), (int(self.position_back[x]),int(self.position_back[y])), (0,0,0), self.line_width)
+
+        # axles
+        self.parallel_axle.draw(image, self.alignment, alignment_wheel, self.position_front[x], self.position_front[y])        
+        self.static_axle.draw(image, self.alignment, self.position_back[x], self.position_back[y])
+
+    def getBarLength(self):
+        return self.length - self.static_axle.wheel.length 
+
+    def __getFrontRadius(self, wheel_alignment):
+        return self.getBarLength() / math.sin(degreeToRadians(wheel_alignment))
+
+    def __getBackRadius(self, wheel_alignment):
+        return self.getBarLength() * math.cos(degreeToRadians(wheel_alignment))
+    
+    def __getDistanceAngle(self, distance, wheel_alignment):
+        if wheel_alignment == 0:
+            distance_angle = 0
+        else:
+            radius_front = self.__getFrontRadius(wheel_alignment)
+            distance_angle = (distance * 180) / (math.pi * radius_front)
+        return distance_angle
+
+    def __getFrontDeviation(self, distance, wheel_alignment):
+        if wheel_alignment == 0:
+            front_deviation = distance
+        else:
+            alpha = 90 - 0.5*self.__getDistanceAngle(distance, wheel_alignment)
+            front_deviation = distance * math.cos(degreeToRadians( (90-alpha) + wheel_alignment))
+        return front_deviation
+    
+    def __getRightDeviation(self, distance, wheel_alignment):
+        if wheel_alignment == 0:
+            right_deviation = 0
+        else:
+            alpha = 90 - 0.5*self.__getDistanceAngle(distance, wheel_alignment)
+            right_deviation = distance * math.sin(degreeToRadians( (90-alpha) + wheel_alignment))
+
+        return right_deviation
+
+    def __getNewFrontPosition(self, current_alignment, front_deviation, right_deviation):
+        front_x = self.position_front[x] + front_deviation * math.cos(degreeToRadians(current_alignment)) - right_deviation * math.sin(degreeToRadians(current_alignment))
+        front_y = self.position_front[y] + front_deviation * math.sin(degreeToRadians(current_alignment)) + right_deviation * math.cos(degreeToRadians(current_alignment))
+        return front_x, front_y
+    
+    def __getBackPosition(self):
+        back_x = self.position_front[x] - self.getBarLength() * math.cos(degreeToRadians(self.alignment))
+        back_y = self.position_front[y] - self.getBarLength() * math.sin(degreeToRadians(self.alignment))
+        return back_x, back_y
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2
+import numpy as np
+
 from vehicle import car_kinematic
 from vehicle import speed
 from vehicle_visual import car_visual
 from vehicle_visual import trailer_visual
+from car import car
 
 
 
@@ -15,6 +18,9 @@ if __name__ == "__main__":
     car_kin = car_kinematic(bar_length)
     car_speed = speed()
     trailor_vis = trailer_visual(200, 100, 40, 20, 2)
+    tatal_car = car(200, 100, 40, 20, 2, 500, 500, 0)
+
+    image = np.ones((1000, 1000, 3), dtype=np.uint8) * 255
 
     wheel_alignment = 0
     bar_alignment = 0
@@ -30,6 +36,7 @@ if __name__ == "__main__":
         wheel_alignment = car_speed.getWheelAlignment()
 
         if (old_alignment != wheel_alignment) or (direction):
+            image = np.ones((1000, 1000, 3), dtype=np.uint8) * 255
             deviation_front = car_kin.getFrontDeviation(direction, wheel_alignment)
             deviation_right = car_kin.getRightDeviation(direction, wheel_alignment)
             deviation_alignment = car_kin.getDistanceAngle(direction, wheel_alignment)
@@ -38,8 +45,12 @@ if __name__ == "__main__":
             bar_alignment = bar_alignment - deviation_alignment
 
             img = car_vis.getImage(-wheel_alignment, front_x, front_y, bar_alignment)
-            trailor_vis.getImage(img, 0, 400, 500, 0)
-            cv2.imshow("image", img)
+            # trailor_vis.getImage(img, 0, 400, 500, 0)
+
+            tatal_car.draw(image, direction, wheel_alignment)
+
+            # cv2.imshow("image", img)
+            cv2.imshow("other", image)
 
 
         old_alignment = wheel_alignment
